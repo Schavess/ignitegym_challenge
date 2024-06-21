5aaa32aa4ab85b14647421500b61dbd905ea2264
Schavess <eng.arthurschaves@gmail.com>
2024-06-21 20:36:03 -0300

    NotificationAndLinkingReady

--- mobile/src/components/Notification.tsx
+++ mobile/src/components/Notification.tsx
@@ -1,39 +1,73 @@
-import { HStack, Text, Icon, IconButton, CloseIcon } from 'native-base';
-
-import { Ionicons } from '@expo/vector-icons';
+import { Pressable, HStack, Text, IconButton, CloseIcon, VStack } from 'native-base';
+import { OSNotification } from 'react-native-onesignal';
+import * as Linking from 'expo-linking';
 
 type Props = {
-  title: String;
+  data: OSNotification;
   onClose: () => void;
 }
 
-export function Notification({ title, onClose }: Props) {
+export function Notification({ data, onClose }: Props) {
+
+  function handleOnPress() {
+    console.log('Notification data:', data);
+
+    let launchURL;
+    if (typeof data.rawPayload === 'string') {
+      try {
+        const payload = JSON.parse(data.rawPayload);
+        const custom = JSON.parse(payload.custom);
+        launchURL = custom.u;
+      } catch (error) {
+        console.error('Error parsing rawPayload:', error);
+      }
+    }
+    console.log('Launch URL:', launchURL);
+
+    if (launchURL) {
+      Linking.openURL(launchURL);
+      onClose();
+    }
+  }
+
   return (
-    <HStack
-      w={'full'}
-      p={4}
+    <Pressable
+      w="100%"
+      p={0}
       pt={12}
-      justifyContent="space-between"
-      alignItems="center"
-      bgColor={"gray.200"}
-      position={'absolute'}
+      bgColor="gray.100"
+      position="absolute"
       top={0}
-    >
-      <Icon as={Ionicons} name='notification-outline' size={5} color={'black'} mr={2} />
+      onPress={handleOnPress}>
+      <HStack
+        w="100%"
+        p={4}
+        pt={12}
+        justifyContent="space-between"
+        alignItems="center"
+        bgColor="gray.200"
+        position="absolute"
+        top={0}
+      >
+        <VStack>
 
-      <Text fontSize={'md'} color={'black'} flex={1}>
-        {title}
-      </Text>
+          <Text fontSize={'md'} fontFamily={'heading'} flex={1}>
+            {data.title}
+          </Text>
+          <Text fontSize={'md'} fontFamily={'body'} flex={1}>
+            {data.body}
+          </Text>
+        </VStack>
 
-      <IconButton
-        variant={'unstyled'}
-        _focus={{ borderWidth: 0 }}
-        icon={<CloseIcon size='3' />}
-        _icon={{ color: 'coolGray.600' }}
-        color={'black'}
-        onPress={onClose}
-      />
-
-    </HStack>
+        <IconButton
+          variant={'unstyled'}
+          _focus={{ borderWidth: 0 }}
+          icon={<CloseIcon size='3' />}
+          _icon={{ color: 'coolGray.600' }}
+          color={'black'}
+          onPress={onClose}
+        />
+      </HStack>
+    </Pressable>
   );
 }
--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,30 +1,98 @@
-import { useContext } from 'react';
-
+import { useContext, useEffect, useState } from 'react';
 import { useTheme, Box } from 'native-base';
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
-
 import { useAuth } from '@hooks/useAuth';
-
 import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from "./app.routes";
 import { Loading } from '@components/Loading';
+import { OneSignal, NotificationWillDisplayEvent, OSNotification } from 'react-native-onesignal';
+import { Notification } from '@components/Notification';
 
 export function Routes() {
-  const { colors } = useTheme();
+  const [notification, setNotification] = useState<OSNotification>();
 
+  const { colors } = useTheme();
   const { user, isLoadingUserStorageData } = useAuth();
+
+  function getLinking() {
+    const prefixes = ['com.datagro.ignitegymchallenge://', 'ignitegymchallenge://', 'exp+ignitegymchallenge://'];
+
+    if (user.id) {
+      return {
+        prefixes,
+        config: {
+          screens: {
+            home: {
+              path: 'home'
+            },
+            exercise: {
+              path: 'exercise/:exerciseId',
+              parse: {
+                exerciseId: (exerciseId: string) => exerciseId
+              }
+            },
+            history: {
+              path: 'history'
+            },
+          },
+        }
+      }
+    } else {
+      return {
+        prefixes,
+        config: {
+          screens: {
+            signIn: {
+              path: 'signin'
+            },
+            signUp: {
+              path: 'signup'
+            },
+          }
+        }
+      }
+    }
+  }
+
+  const linking = getLinking();
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
 
-  if(isLoadingUserStorageData) {
+  if (isLoadingUserStorageData) {
     return <Loading />
+  }
+
+  useEffect(() => {
+    const handleNotification = (event: NotificationWillDisplayEvent): void => {
+      event.preventDefault();
+      const response = event.getNotification();
+      setNotification(response);
+    }
+
+    OneSignal.Notifications.addEventListener("foregroundWillDisplay", handleNotification);
+
+    return () => OneSignal.Notifications.removeEventListener("foregroundWillDisplay", handleNotification);
+  }, []);
+
+  function handleNotificationClose() {
+    setNotification(undefined);
   }
 
   return (
     <Box flex={1} bg="gray.700">
-      <NavigationContainer theme={theme}>
-        {user.id ? <AppRoutes /> : <AuthRoutes />}
+      <NavigationContainer theme={theme} linking={linking}>
+        {user.id ?
+          <>
+            <AppRoutes />
+            {notification?.title &&
+              (
+                <Notification data={notification} onClose={handleNotificationClose} />
+              )
+            }
+          </>
+          :
+          <AuthRoutes />}
       </NavigationContainer>
     </Box>
   );
