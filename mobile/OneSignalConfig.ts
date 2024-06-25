fdcee110c0b929e1162de9127569294533a90e99
Schavess <eng.arthurschaves@gmail.com>
2024-06-25 15:42:00 -0300

    backendNotificationJustWithAxios_rm

--- mobile/OneSignalConfig.ts
+++
@@ -1,2 +0,0 @@
-export const apiBaseURL = "172.17.0.1:3333" //Default Port is 3333, so baseUrl must be your IP + port, example 123.123.1.123:3333
-export const oneSignalKey = "c1453403-aa75-4649-bf2a-27eabe8594c7"
